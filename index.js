0ba235c4c61b714e3200f80d82a02c79e007b06c
zacanger <zac@zacanger.com>
2019-07-13 10:04:44 -0600

    Clean it up

--- index.js
+++ index.js
@@ -3,14 +3,12 @@
 const fs = require('fs')
 
 const rmrf = (d) => {
-  const files = fs.readdirSync(d)
-  if (files && files.length > 0) {
-    files.forEach((file) => {
+  fs.readdirSync(d)
+    .forEach((file) => {
       const fp = d + '/' + file
       if (fs.lstatSync(filePath).isDirectory()) rmrf(fp)
       else fs.unlinkSync(fp)
     })
-  }
   fs.rmdirSync(d)
 }
 
